a991688bf49e4cca87a2b5155080b565d964b160
gt2017 <gt2017qd@163.com>
2018-04-28 23:28:09 +0800

    add B1001

---
+++ B1001/B1001.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    // printf("hello world!");
+    int count = 0;
+    int n = 0;
+    scanf('%d', in);
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+            n = n / 2;
+        else
+            n = 3 * n + 1;
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # try to finish all the questions !
+
+OOps! pat的nodejs版本为0.10.33, 目前测试支持es5的语法, 不支持es6的语法, 比如let, 或者箭头函数
