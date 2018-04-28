d7ae3a8e36ae0ea5bd8f06e765c443ab3e84a274
gt2017 <gt2017qd@163.com>
2018-04-28 23:52:39 +0800

    Update B1001.cpp

--- B1001/B1001.cpp
+++ B1001/B1001.cpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
-int main()
-{
+int main(){
     // printf("hello world!");
     int count = 0;
     int n = 0;
-    scanf('%d', in);
-    while (n != 1)
-    {
+    scanf("%d", &n);
+    while (n != 1){
+    	count++;
         if (n % 2 == 0)
             n = n / 2;
         else
-            n = 3 * n + 1;
+            n = (3*n+1)/2;
     }
+    printf("%d",count);
     return 0;
 }
